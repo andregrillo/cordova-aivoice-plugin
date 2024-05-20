0c77ea7165e558e5d6a82db0392e7bcd3c55a689
André Grillo <andregrillo@gmail.com>
2024-05-20 10:23:22 +0100

    JS methods updated

--- www/CdvAiVoice.js
+++ www/CdvAiVoice.js
@@ -1,5 +1,13 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (success, error, arg0) {
-    exec(success, error, 'CdvAiVoice', 'coolMethod', [arg0]);
+exports.startListening = function (success, error) {
+    exec(success, error, 'CdvAiVoice', 'startListening');
+};
+
+exports.stopListening = function (success, error) {
+    exec(success, error, 'CdvAiVoice', 'stopListening');
+};
+
+exports.speak = function (success, error, text) {
+    exec(success, error, 'CdvAiVoice', 'speak', [text]);
 };
